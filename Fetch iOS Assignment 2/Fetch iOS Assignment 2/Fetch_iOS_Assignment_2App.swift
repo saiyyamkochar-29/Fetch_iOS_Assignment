dc1b94ef0e7d671f0d7eb61224cac7cbf5e2615a
saiyyamkochar-29 <69192707+saiyyamkochar-29@users.noreply.github.com>
2023-07-10 01:11:27 -0400

    Add files via upload

---
+++ Fetch iOS Assignment 2/Fetch iOS Assignment 2/Fetch_iOS_Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_Assignment_2App.swift
+//  Fetch iOS Assignment 2
+//
+//  Created by Saiyyam Kochar on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
